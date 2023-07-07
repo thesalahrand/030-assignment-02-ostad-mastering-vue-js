40075209f40dc835dc6a5c3d6a6c43e2d3d5cdec
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-07-07 20:06:08 +0600

    ready to be submitted #2

--- 6.js
+++ 6.js
@@ -1,3 +1,5 @@
-const getLargestNumber = (a, b) => (a > b && a) || (a < b && b)
+const getLargestNumber = (a, b) => (a >= b && a) || (a < b && b)
 
 console.log(getLargestNumber(10, 5))
+console.log(getLargestNumber(10, 10))
+console.log(getLargestNumber(5, 10))
